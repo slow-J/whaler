61c990c483625b14ad9bd34a4c2b69686b3f9783
Jakub Slowinski <32519034+slow-J@users.noreply.github.com>
2020-03-13 18:56:15 +0000

    Update get-whaler.sh

--- get-whaler.sh
+++ get-whaler.sh
@@ -2,7 +2,7 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 usermod -aG docker ubuntu
 apt-get install -y docker-compose
-git clone https://github.com/oncyberblog/whaler.git
+git clone https://github.com/slow-J/whaler.git
 systemctl enable docker
 apt-get update && apt-get upgrade -y
 reboot now
