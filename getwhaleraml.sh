491e0a795134905b2d50177f12ae855ba8ffa031
Jakub Slowinski <32519034+slow-J@users.noreply.github.com>
2020-03-16 16:49:12 +0000

    Update getwhaleraml.sh

--- getwhaleraml.sh
+++ getwhaleraml.sh
@@ -1,14 +1,11 @@
-yum install -y yum-utils device-mapper-persistent-data lvm2
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce
-usermod -aG docker $(whoami)
-systemctl enable docker.service
-systemctl start docker.service
-yum install epel-release
-yum install -y python-pip
-pip install docker-compose
-yum upgrade python*
-yum update && yum upgrade -y
-git clone https://github.com/slow-J/whaler.git
+yum install docker
+service docker start
+usermod -a -G docker ec2-user
+chkconfig docker on
+yum install -y git
+curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+git clone https://github.com/slow-J/whaler.git                                              
 # reboot after cloudformation script finished
 # reboot now
